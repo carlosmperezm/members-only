54d90542672de3e8d81c60bfaacf71a81e8c53c2
CarlosPerez <carlosmperez2901@gmail.com>
2026-01-09 13:57:48 -0800

    Add queries to create the database's tables and rows

---
+++ db/seedQueries.js
@@ -0,0 +1,37 @@
+
+export const CREATE_USERS_TABLE = `
+    CREATE TABLE IF NOT EXITS users(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        first_name VARCHAR(100),
+        last_name VARCHAR(100),
+        password VARCHAR(255),
+        member BOOLEAN,
+        admin BOOLEAN
+    );
+`;
+
+export const CREATE_MESSAGES_TABLE = `
+    CREATE TABLE IF NOT EXITS messages(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        title VARCHAR(255) NOT NULL,
+        date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        description TEXT,
+        author INT REFERENCES users(id) ON DELETE SET NULL
+    );
+`;
+
+export const CREATE_USERS = `
+    INSERT INTO users(first_name, last_name, password, member, admin)
+        VALUES('John', 'Doe', 'mySuperSecretPassword', TRUE, FALSE),
+        VALUES('Rick', 'Dominic','rickPassword', TRUE, TRUE),
+        VALUES('Laura', 'Hernandez', 'lauraPass', FALSE, FALSE)
+        ;
+`;
+
+export const CREATE_MESSAGES = `
+    INSERT INTO messages(title, description, author)
+        VALUES('My super message', 'Some random description', 1 ),
+        VALUES('My title', 'Some random description', 2 ),
+        VALUES('My local title', 'Some random description', 1 )
+        ;
+`;
